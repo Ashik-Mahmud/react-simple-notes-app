e2220b8c4fa927810684774478738ce61a4086c5
ashikmahmud <ashikmamud187@gmail.com>
2022-04-11 02:57:05 +0600

    do responsive of my application called notes app

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,39 +1,12 @@
 import styled from "@emotion/styled";
-import FileCopyIcon from "@mui/icons-material/FileCopyOutlined";
-import PrintIcon from "@mui/icons-material/Print";
-import SaveIcon from "@mui/icons-material/Save";
-import ShareIcon from "@mui/icons-material/Share";
-import { SpeedDial, SpeedDialAction, SpeedDialIcon } from "@mui/material";
-import { Box } from "@mui/system";
 import React from "react";
 import { Toaster } from "react-hot-toast";
 import Notes from "../Notes/Notes";
 const Home = () => {
-  const actions = [
-    { icon: <FileCopyIcon />, name: "Copy" },
-    { icon: <SaveIcon />, name: "Save" },
-    { icon: <PrintIcon />, name: "Print" },
-    { icon: <ShareIcon />, name: "Share" },
-  ];
   return (
     <>
       <Toaster />
       <HomeContainer>
-        <Box>
-          <SpeedDial
-            ariaLabel="SpeedDial basic example"
-            sx={{ position: "absolute", top: 500, right: 50 }}
-            icon={<SpeedDialIcon />}
-          >
-            {actions.map((action) => (
-              <SpeedDialAction
-                key={action.name}
-                icon={action.icon}
-                tooltipTitle={action.name}
-              />
-            ))}
-          </SpeedDial>
-        </Box>
         <Notes />
       </HomeContainer>
     </>
@@ -41,5 +14,8 @@ const Home = () => {
 };
 const HomeContainer = styled.section`
   padding: 3rem;
+  @media (max-width: 600px) {
+    padding: 1rem;
+  }
 `;
 export default Home;
